f43774494637336185bb846d4e726f08be749337
tumlinson@stsci.edu <tumlinson@stsci.edu>
2024-01-08 21:04:40 -0600

    deleted templates_old for camera

--- camera_etc/hdi_help.py
+++ camera_etc/hdi_help.py
@@ -13,7 +13,7 @@ def help():
         </div>
         <div class="col-lg-6">
         <p> 
-          <p>The details of these calculations are <a href="http://jt-astro.science/luvoir_simtools/hdi_etc/SNR_equation.pdf" target="_blank"> here</a>. We assume that the pixel size in each band critically samples the telescope's diffraction limited PSF at the shortest wavelength in that channel. Thermal backgrounds are included for T = 280 K, which substantially affects the K band.</p>
+          <p>The details of these calculations are <a href="http://jt-astro.science/luvoir_simtools/hdi_etc/SNR_equation.pdf" target="_blank"> here</a>. We assume that the pixel size in each band critically samples the telescope's diffraction limited PSF at the shortest wavelength in that channel. Thermal backgrounds are computed for T = 280 K, which substantially affects the K band.</p>
         </div>
       </div>
           """

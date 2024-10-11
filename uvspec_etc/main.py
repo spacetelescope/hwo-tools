1f3950c9f23ea98177e4c3a007beb51f828d109b
tumlinson@stsci.edu <tumlinson@stsci.edu>
2024-10-11 18:12:10 -0400

    upadtes

--- uvspec_etc/main.py
+++ uvspec_etc/main.py
@@ -13,121 +13,82 @@ import uvi_help as h
 import astropy.units as u
 from syotools.models import Telescope, Spectrograph
 
-# create a Telescope, Camera, and Exposure 
+# create Telescope, Camera, and Exposure objects 
 hwo, uvi = Telescope(), Spectrograph() 
 hwo.set_from_json('EAC3')
 hwo.add_spectrograph(uvi)
 uvi_exp = uvi.create_exposure()
-uvi.mode='G120M' 
+uvi.mode='G150M' 
 
-#spec_dict = get_uvi_spectra.add_spectrum_to_library() # obtain a dictionary with the pysynphot spectra in it  
 template_to_start_with = 'QSO' 
 uvi_exp.sed_id = template_to_start_with 
 uvi_exp.renorm_sed(21. * u.ABmag, bandpass='galex,fuv', waveunits='angstrom', fluxunits='flam')
-uvi_exp.exptime[1]['value'] = [1.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0]
+uvi_exp.exptime[1]['value'] = [1.0*3600., 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0]
 
-uvi_exp.verbose = True
-hwo.verbose = True
-snr = uvi_exp.recover('snr')
 uvi_exp.enable()
 uvi_sed, uvi_snr = uvi_exp.recover('sed', 'snr')
 
 spectrum_template = ColumnDataSource(data=dict(w=uvi_exp.exp_wave, f=uvi_exp.exp_flux, sn=uvi_exp.exp_snr)) 
 instrument_info = ColumnDataSource(data=dict(wave=uvi.wave, bef=uvi.bef))
+sed_cds = ColumnDataSource(data=dict(w=uvi_exp._sed.wave, f=uvi_exp._sed.flux)) 
 
 # set up the flux plot 
 flux_plot = figure(height=400, width=800, 
               tools="crosshair,hover,pan,reset,save,box_zoom,wheel_zoom", outline_line_color='black', 
-              x_range=[900, 2000], y_range=[0, 4e-16], toolbar_location='right') 
-flux_plot.x_range=Range1d(900,3000,bounds=(900,3000))
-flux_plot.y_range=Range1d(0,4e-16,bounds=(0,None)) 
-flux_plot.yaxis.axis_label = 'Flux [erg / s / cm^2 / Ang]' 
-flux_plot.xaxis.axis_label = 'Wavelength [Angstrom]' 
-flux_plot.line('w', 'f', source=spectrum_template, line_width=3, line_color='firebrick', line_alpha=0.7, legend_label='Source Flux')
+              x_range=[900, 2000], y_range=[0, 1e-14], toolbar_location='right') 
+flux_plot.x_range, flux_plot.y_range = Range1d(900,3000,bounds=(900,3000)), Range1d(0,1e-14,bounds=(0,None)) 
+flux_plot.yaxis.axis_label, flux_plot.xaxis.axis_label = 'Flux [erg / s / cm^2 / Ang]', 'Wavelength [Angstrom]' 
+flux_plot.line('w', 'f', source=sed_cds, line_width=2, line_color='cyan', line_alpha=0.7, legend_label='Source SED')
+flux_plot.line('w', 'f', source=spectrum_template, line_width=3, line_color='firebrick', line_alpha=0.7, legend_label='Observed Flux')
 flux_plot.line('wave', 'bef', source=instrument_info, line_width=3, line_color='darksalmon', line_alpha=0.7, legend_label='Background')
 
 # set up the snr plot 
 sn_plot = figure(height=400, width=800, 
               tools="crosshair,hover,pan,reset,save,box_zoom,wheel_zoom", outline_line_color='black', 
-              x_range=[900, 2000], y_range=[0, 40], toolbar_location='right')
+              x_range=[900, 2000], y_range=[0, 25], toolbar_location='right')
 sn_plot.x_range=Range1d(900,3000,bounds=(900,3000))
-sn_plot.y_range=Range1d(0,40,bounds=(0,None)) 
+sn_plot.y_range=Range1d(0,25,bounds=(0,None)) 
 sn_plot.line('w', 'sn', source=spectrum_template, line_width=3, line_color='orange', line_alpha=0.7, legend_label='S/N per resel')
 sn_plot.xaxis.axis_label = 'Wavelength [Angstrom]' 
 sn_plot.yaxis.axis_label = 'S/N per resel' 
 sn_plot.yaxis.major_label_overrides = {0:' ', 5:'                5'} 
 
-
 def update_cds(attrname, old, new): # use this one for updating pysynphot templates 
 
     print("You have chosen template ", template.value) 
-    print('Selected grating = ', grating.value)
     hwo.aperture = aperture.value
     print('Your telescope is set to', hwo.aperture)
-    uvi.mode = grating.value
+    print('Selected grating = ', grating.value,  uvi.list_of_gratings[grating.value]) 
+    uvi.mode = uvi.list_of_gratings[grating.value] 
+    print(' ') 
+
+    uvi_sed, uvi_snr = uvi_exp.recover('sed', 'snr')
 
     uvi_exp.sed_id = template.value 
     uvi_exp.renorm_sed(magnitude.value * u.ABmag, bandpass='galex,fuv', waveunits='angstrom', fluxunits='flam')
-    uvi_exp.exptime[1]['value'] = [exptime.value, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0]
+    uvi_exp.exptime[1]['value'] = [exptime.value * 3600., 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0]
     
-    snr = uvi_exp.recover('snr')
-    uvi_exp.enable()
     uvi_sed, uvi_snr = uvi_exp.recover('sed', 'snr')
+
+    #need to enable extinction here also, probablu inside SYOTools 
     
+    if ('Blackbody' in template.value):      #<---- update the blackbody curve here. 
+       print(" Blackbody not implemented yet") 
+       #bb = S.BlackBody(bb_temperature.value) 
+       #bb.convert('flam')
+       #bb_renorm = bb.renorm(magnitude.value, 'abmag', S.ObsBandpass('galex,fuv')) 
+       #spec_with_extinction = bb_renorm * S.Extinction(reddening.value, 'mwavg')  
+  
     spectrum_template.data = dict(w=uvi_exp.exp_wave, f=uvi_exp.exp_flux, sn=uvi_exp.exp_snr)
     instrument_info = ColumnDataSource(data=dict(wave=uvi.wave, bef=uvi.bef))
+    sed_cds.data = dict(w=uvi_exp._sed.wave, f=uvi_exp._sed.flux) 
 
-    #set the axes to autoscale appropriately 
-    flux_plot.y_range.start = 0 
+    flux_plot.y_range.start = 0   #   set the axes to autoscale appropriately 
     flux_plot.y_range.end = 1.5*np.max(np.array(uvi_exp.exp_flux)) 
-    sn_plot.y_range.start = 0
-    #sn_plot.y_range.end = 1.3*np.max(np.array(uvi_exp.exp_snr)) 
 
-def update_data(attrname, old, new): # use this one for updating pysynphot templates 
-   
-    print("You have chosen template ", template.value, np.size(spec_dict[template.value].wave)) 
-    print('Selected grating = ', grating.value) 
-    hwo.aperture = aperture.value 
-    print('Your telescope is set to', hwo.aperture) 
-    uvi.mode = grating.value  
-
-    print('Applying reddening for E(B-V)') 
-    spec_with_extinction = spec_dict[template.value] * S.Extinction(reddening.value, 'mwavg')  
-
-    if ('Blackbody' in template.value):      #<---- update the blackbody curve here. 
-       bb = S.BlackBody(bb_temperature.value) 
-       bb.convert('flam')
-       bb_renorm = bb.renorm(21., 'abmag', S.ObsBandpass('galex,fuv')) 
-       spec_with_extinction = bb_renorm * S.Extinction(reddening.value, 'mwavg')  
- 
-    spec_with_extinction_renorm = spec_with_extinction.renorm(21., 'abmag', S.ObsBandpass('galex,fuv')) 
-
-    new_w0 = spec_with_extinction_renorm.wave 
-    new_f0 = spec_with_extinction_renorm.flux 
-
-    #OOPS, SHOULD USE PYSYNPHOT FOR REDSHIFT HERE, THE NORMALIZATION IS NOT QUITE CORRECT 
-    new_w = np.array(new_w0) * (1. + redshift.value)
-    new_f = np.array(new_f0) * 10.**( (21.-magnitude.value) / 2.5)
-    new_sn = np.nan_to_num(simulate_exposure(hwo, uvi, new_w, new_f, exptime.value)) 
-
-    flux_cut = copy.deepcopy(new_f) 
-    flux_cut[new_w < uvi.wrange[0]] = -999.  
-    flux_cut[new_w > uvi.wrange[1]] = -999.  
-    print('RANGE', uvi.wrange[0], uvi.wrange[1]) 
-
-    new_dict = {'w':new_w, 'f':new_f, 'w0':new_w0, 'f0':new_f0, 'flux_cut':flux_cut, 'sn':new_sn} 
-    spectrum_template.data = new_dict 
-
-    # set the axes to autoscale appropriately 
-    flux_plot.y_range.start = 0 
-    flux_plot.y_range.end = 1.5*np.max(flux_cut)
-    sn_plot.y_range.start = 0 
-    sn_plot.y_range.end = 1.3*np.max(spectrum_template.data['sn'])
-    print('MAX MAX', np.max(spectrum_template.data['f']), np.max(flux_cut)) 
 
 # fake source for managing callbacks 
 source = ColumnDataSource(data=dict(value=[]))
-#source.on_change('data', update_data)
 source.on_change('data', update_cds)
 
 # Set up widgets and their callbacks (faking the mouseup policy via "source" b/c functional callback doesn't do that. 
@@ -179,7 +140,6 @@ exptime_callback = CustomJS(args=dict(source=source), code="""
 exptime.js_on_change("value_throttled", exptime_callback) 
 
 # iterate on changes to parameters 
-#for w in [template, grating]:  w.on_change('value', update_data)
 for w in [template, grating]:  w.on_change('value', update_cds)
  
 # Set up the layouts and add to document
